cc1a051e9a4153d1df99b0a3271eb2fb60118513
Philip Wee <philip.andrewweedewang@gmail.com>
2021-08-15 09:42:39 +0800

    Removed cli-wrapper

--- packages/cli/frameworks/common/config.ts
+++ packages/cli/frameworks/common/config.ts
@@ -1,6 +1,21 @@
+import path from 'path'
+
 export default {
-    plugins: [
-        "@ezbackend/common",
-        "@ezbackend/openapi"
-    ]
-}
+  port: 8888,
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 8888,
+    logger: {
+      prettyPrint: {
+        translateTime: "SYS:HH:MM:ss",
+        ignore: "pid,hostname,reqId,responseTime,req,res",
+        messageFormat: "[{req.method} {req.url}] {msg}",
+      },
+    },
+  },
+  orm: {
+    logging: false,
+  },
+  plugins: ["@ezbackend/common","@ezbackend/openapi"],
+  entryPoint: path.resolve(__dirname,"./test.index.ts"),
+  connectionURI: "sqlite::memory"
+};
--- packages/cli/frameworks/common/index.ts
+++ packages/cli/frameworks/common/index.ts
@@ -1,86 +1,20 @@
 import { EzModel, EzRouter } from "@ezbackend/common";
 import { DataTypes } from "sequelize";
-import _ from "lodash";
-
-export const sample = new EzModel("sample", {
-  string: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  int: {
-    type: DataTypes.INTEGER,
-  },
-  float: {
-    type: DataTypes.FLOAT,
-  },
-  double: {
-    type: DataTypes.DOUBLE
-  },
-  real: {
-    type: DataTypes.REAL
-  },
-  enum: {
-    type: DataTypes.ENUM,
-    values: ['one','two','three']
-  },
-  data: {
-    type: DataTypes.DATE
-  },
-  array: {
-    type: DataTypes.ARRAY(DataTypes.TEXT)
-  },
-  uuid: {
-    type: DataTypes.UUID
-  },
-  json: {
-    type: DataTypes.JSON
-  }
-});
 
 export const user = new EzModel("user", {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  age: {
+    type: DataTypes.INTEGER
+  }
 });
 
-export const program = new EzModel("program", {});
+export const framework = new EzModel("framework", {
+  name: {
+    type: DataTypes.STRING
+  }
+});
 
-program.hasOne(user);
-
-// export const customEndpoint = new EzRouter("custom")
-
-// customEndpoint.registerRoute({
-//   method: 'GET',
-//   url: '/',
-//   schema: {
-//     body: {
-
-//     },
-//     response: {
-//       200: {}
-//     }
-//   },
-//   handler(req,res) {
-//     res.send('hi')
-//   }
-// })
-
-  
-// export const customEndpoint = new EzRouter("custom")
-
-// customEndpoint.registerRoute({
-//   method: 'GET',
-//   url: '/',
-//   schema: {
-//     body: {
-
-//     },
-//     response: {
-//       200: {}
-//     }
-//   },
-//   handler(req,res) {
-//     res.send('hi')
-//   }
-// })
+user.hasOne(framework);
