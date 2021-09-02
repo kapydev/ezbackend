893f5f3f59c4d588f0d9eca00e62e480f89aef1f
Philip Wee <philip.andrewweedewang@gmail.com>
2021-09-02 15:05:04 +0800

    Updated tests
    Although some are failing

--- packages/common/test/common-nested.test.ts
+++ packages/common/test/common-nested.test.ts
@@ -5,7 +5,7 @@ import "../src"
 
 beforeAll(async () => {
   const ezb = EzBackend.app()
-  ezb.plugins.run = (ezb,opts,cb) => {cb()}
+  ezb.plugins.run = (ezb, opts, cb) => { cb() }
   await EzBackend.start(path.resolve(__dirname, "test.config.ts"));
 });
 
@@ -21,29 +21,43 @@ const sampleProgram = {
     {
       name: "Robert",
       detail: {
-        age: 20,
-      },
+        age: 27
+      }
+
     },
   ],
 };
 
 describe("Nested CRUD", () => {
   describe("Create", () => {
-    test("Nested creation", async () => {
+    test("Cascade creation", async () => {
       const ezb = EzBackend.app() as EzBackend;
       const response = await ezb.server.inject({
         method: "POST",
         url: "/Program",
         payload: sampleProgram,
       });
-      
+
       expect(response.statusCode).toEqual(200);
       expect(JSON.parse(response.body)).toMatchObject(sampleProgram);
       expect(JSON.parse(response.body)).toHaveProperty("id");
     })
+
+    test("No cascade creation", async () => {
+      const ezb = EzBackend.app() as EzBackend;
+      const response = await ezb.server.inject({
+        method: "POST",
+        url: "/NoCascadeProgram",
+        payload: sampleProgram,
+      });
+      //NOTE: The current implementation does not throw an error when additional fields are specified, they are just ignored
+      expect(response.statusCode).toEqual(200);
+      expect(JSON.parse(response.body)).toMatchObject({ name: "My first program" });
+      expect(JSON.parse(response.body)).toHaveProperty("id");
+    })
   });
   describe("Read", () => {
-    test("Basic read", async () => {
+    test("Eager Loading", async () => {
       const ezb = EzBackend.app() as EzBackend;
       const response = await ezb.server.inject({
         method: "GET",
@@ -55,5 +69,121 @@ describe("Nested CRUD", () => {
       expect(JSON.parse(response.body)).toMatchObject(sampleProgram);
       expect(JSON.parse(response.body)).toHaveProperty("id");
     })
+
+    test("Lazy Loading", async () => {
+      const ezb = EzBackend.app() as EzBackend;
+      const response = await ezb.server.inject({
+        method: "GET",
+        url: "/NoCascadeProgram/1",
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(JSON.parse(response.body)).toMatchObject({ name: "My first program" });
+      expect(JSON.parse(response.body)).toHaveProperty("id");
+    })
+
   });
+
+  describe("Update", () => {
+    test("Cascade Update", async () => {
+      const ezb = EzBackend.app() as EzBackend;
+      const response = await ezb.server.inject({
+        method: "PATCH",
+        url: "/Program/1",
+        payload: {
+          name: "My first program",
+          users: [
+            {
+              name: "Thomas",
+            },
+            {
+              name: "Holly",
+            },
+          ],
+        }
+      });
+
+      expect(response.statusCode).toEqual(200);
+
+      expect(JSON.parse(response.body)).toMatchObject({
+        name: "My first program",
+        users: [
+          {
+            name: "Thomas",
+          },
+          {
+            name: "Holly",
+          },
+        ],
+      });
+      expect(JSON.parse(response.body)).toHaveProperty("id");
+    })
+
+    describe("Foreign Key ID", () => {
+      test("Create with Foreign Key ID", async () => {
+        const ezb = EzBackend.app() as EzBackend;
+        
+        const response = await ezb.server.inject({
+          method: "POST",
+          url: "/NoCascadeUser",
+          payload: {
+            name: "Willip Pee",
+            programId: 1
+          }
+        });
+
+        expect(response.statusCode).toEqual(200);
+
+        expect(JSON.parse(response.body)).toMatchObject({
+          name: "Willip Pee",
+          programId: 1
+        });
+        expect(JSON.parse(response.body)).toHaveProperty("id");
+      })
+
+      test("Update with Foreign Key ID", async () => {
+        const ezb = EzBackend.app() as EzBackend;
+        
+        const response = await ezb.server.inject({
+          method: "PATCH",
+          url: "/NoCascadeUser/1",
+          payload: {
+            programId: 2
+          }
+        });
+        console.log(response.body)
+        expect(response.statusCode).toEqual(200);
+
+        expect(JSON.parse(response.body)).toMatchObject({
+          name: "Willip Pee",
+          programId: 2
+        });
+        expect(JSON.parse(response.body)).toHaveProperty("id");
+      })
+      test("Get entity with just foreign key IDs", async () => {
+        const ezb = EzBackend.app() as EzBackend;
+        
+        const response = await ezb.server.inject({
+          method: "GET",
+          url: "/NoCascadeUser/1"
+        });
+
+        expect(response.statusCode).toEqual(200);
+
+        expect(JSON.parse(response.body)).toMatchObject({
+          name: "Willip Pee",
+          programId: 2
+        });
+        expect(JSON.parse(response.body)).toHaveProperty("id");
+      })
+
+    })
+  })
+
+  describe("Delete", () => {
+    test("Cascade Delete", async () => {
+
+    })
+  })
+
 });
--- packages/common/test/test.index.ts
+++ packages/common/test/test.index.ts
@@ -1,4 +1,134 @@
-import { EzModel } from "../src";
+// import { EzModel } from "../src";
+// import {
+//   PrimaryGeneratedColumn,
+//   Column,
+//   OneToOne,
+//   JoinColumn,
+//   OneToMany,
+//   ManyToOne,
+// } from "typeorm";
+
+// @EzModel()
+// export class Sample {
+//   @PrimaryGeneratedColumn()
+//   id: number
+  
+//   @Column({
+//     type: "varchar"
+//   })
+//   varchar: string
+
+//   @Column({
+//     type: "integer"
+//   })
+//   int: number
+
+//   @Column({
+//     type: "float"
+//   })
+//   float: number
+
+//   @Column({
+//     type: "double"
+//   })
+//   double: number
+
+//   @Column({
+//     type: "real"
+//   })
+//   real: number
+
+//   @Column({
+//     type: "date"
+//   })
+//   date: string
+
+//   @Column("simple-json")
+//   json: {field1: string, field2: number}
+// }
+
+// @EzModel()
+// export class Program {
+
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column()
+//   name: string 
+
+//   @OneToMany(type => User, user => user.program, {
+//     cascade: true,
+//     eager: true
+//   })
+//   users: User[]
+// }
+
+// @EzModel()
+// export class NoCascadeProgram {
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column()
+//   name: string
+
+//   @OneToMany(type => NoCascadeUser, user => user.program)
+//   users: User[]
+// }
+
+// @EzModel()
+// export class NoCascadeUser {
+
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column()
+//   name: string
+
+//   @OneToOne(type => Detail)
+//   @JoinColumn()
+//   detail: Detail
+
+//   @ManyToOne(type => NoCascadeProgram, program => program.users)
+//   program: Program
+
+//   @Column({
+//     nullable: true
+//   })
+//   programId: number
+// }
+
+
+// @EzModel()
+// export class Detail {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   age: number;
+// }
+
+// @EzModel()
+// export class User {
+
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column()
+//   name: string
+
+//   @OneToOne(type => Detail, {
+//     cascade: true,
+//     eager: true
+//   })
+//   @JoinColumn()
+//   detail: Detail
+
+//   @ManyToOne(type => Program, program => program.users)
+//   program: Program
+// }
+
+
+import { EzModel } from "@ezbackend/common";
 import {
   PrimaryGeneratedColumn,
   Column,
@@ -64,6 +194,18 @@ export class Program {
 }
 
 @EzModel()
+export class NoCascadeProgram {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @OneToMany(type => NoCascadeUser, user => user.program)
+  users: User[]
+}
+
+@EzModel()
 export class Detail {
   @PrimaryGeneratedColumn()
   id: number;
@@ -71,6 +213,32 @@ export class Detail {
   @Column()
   age: number;
 }
+
+@EzModel()
+export class NoCascadeUser {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @OneToOne(type => Detail)
+  @JoinColumn()
+  detail: Detail
+
+  @ManyToOne(type => NoCascadeProgram, program => program.users)
+  program: Program
+  
+  //LEFT OFF: Why is this not in the create schema?
+  @Column({
+    nullable: true
+  })
+  programId: number
+}
+
+
+
 
 @EzModel()
 export class User {
