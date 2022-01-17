d6c1f974c4e7f22a849d43d3c0e7e2cf7a9ab963
Stephen Alvin <stephenalvin0912@gmail.com>
2022-01-18 04:04:10 +0800

    Added selection capability for drawerItems

--- packages/db-ui/ezbackend-database-ui/src/Components/DrawerItem.tsx
+++ packages/db-ui/ezbackend-database-ui/src/Components/DrawerItem.tsx
@@ -1,14 +1,28 @@
 import { ListItem, Typography, Box } from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
 
 interface IDrawerItem {
   name: string;
+  route: string;
 }
 
 function DrawerItem(props: IDrawerItem) {
+  const location = useLocation();
   return (
     <ListItem button={true}>
       <Typography>
-        <Box color="white" fontFamily="monospace">
+        <Box
+          color={location.pathname === props.route ? '#1C2023' : 'white'}
+          fontFamily="monospace"
+          padding={1}
+          paddingX={2}
+          borderRadius={6}
+          fontWeight={location.pathname === props.route ? 'bold' : 'normal'}
+          style={{
+            backgroundColor:
+              location.pathname === props.route ? 'white' : '#1C2023',
+          }}
+        >
           {props.name}
         </Box>
       </Typography>
--- packages/db-ui/ezbackend-database-ui/src/Components/Layout.tsx
+++ packages/db-ui/ezbackend-database-ui/src/Components/Layout.tsx
@@ -14,21 +14,45 @@ import Database from '../Pages/Database';
 import Docs from '../Pages/Docs';
 import SocketIODocs from '../Pages/SocketIODocs';
 import openInNewTab from '../Utils/openInNewTab';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+} from 'react-router-dom';
 
-const drawerItems = (
-  <>
-    <Link to="/" style={{ textDecoration: 'none' }}>
-      <DrawerItem name="Database" />
+interface ILinkItem {
+  route: string;
+  name: string;
+}
+
+function LinkItem(props: ILinkItem) {
+  return (
+    <Link to={props.route} style={{ textDecoration: 'none' }}>
+      <DrawerItem name={props.name} route={props.route} />
     </Link>
-    <Link to="/api-documentation" style={{ textDecoration: 'none' }}>
-      <DrawerItem name="API Docs" />
-    </Link>
-    <Link to="/socket-io-documentation" style={{ textDecoration: 'none' }}>
-      <DrawerItem name="Socket IO Docs" />
-    </Link>
-  </>
-);
+  );
+}
+
+const drawerItemContents = [
+  {
+    route: '/',
+    name: 'Database',
+  },
+  {
+    route: '/api-documentation',
+    name: 'API Docs',
+  },
+  {
+    route: '/socket-io-documentation',
+    name: 'Socket IO Docs',
+  },
+];
+
+const drawerItems = drawerItemContents.map((drawerItemContent) => (
+  <LinkItem route={drawerItemContent.route} name={drawerItemContent.name} />
+));
 
 interface ILayout {
   open: boolean;
@@ -52,27 +76,6 @@ function Layout(props: ILayout) {
               style={{ backgroundColor: '#1C2023' }}
             >
               <Toolbar>
-                {/* For Mobile Only */}
-                <Hidden smUp>
-                  <IconButton
-                    edge="start"
-                    color="inherit"
-                    onClick={props.handleOpen}
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                </Hidden>
-
-                {/* For Mobile Only */}
-                <Hidden smUp>
-                  <IconButton
-                    edge="start"
-                    color="inherit"
-                    onClick={props.handleOpen}
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                </Hidden>
 
                 <Button
                   style={{ textTransform: 'none' }}
